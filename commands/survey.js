dc29c0b3a23aaf3a121c758a5fd5f9a6e179f62c
Brian Morrison II <brian@brianmorrison.me>
2022-05-06 10:01:40 -0500

    Refactor survey.js to use async/await

--- commands/survey.js
+++ commands/survey.js
@@ -16,57 +16,46 @@ module.exports = {
 		.setName('survey')
 		.setDescription('Starts a survey!'),
 	async execute(interaction) {
-		const survey = [];
-		
-
+		const survey = {};
 		const filter = m => { return interaction.user.id === m.author.id };
 
-		interaction.reply('Welcome to the survey command. Firstly, what is your name?', { fetchReply: true })
-			.then(() => {
-				interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-					.then(collected => {
-						survey.name = `${collected.first().content}`
-						//console.log(survey)
-						interaction.followUp(`What is your favorite candy?`)
-							.then(() => {
-								interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-									.then(collected => {
-										survey.candy = `${collected.first().content}`
-										//console.log(survey)
-										interaction.followUp(`I'm not sure I like that candy... Why do you enjoy it so?`)
-											.then(() => {
-												interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-													.then(collected => {
-														survey.reason = `${collected.first().content}`
-														interaction.followUp(`Fair enough! That's as good a reason as any!`)
+		try {
+			await interaction.reply('Welcome to the survey command. Firstly, what is your name?', { fetchReply: true })
 
-														//console.log(survey)
+			let collected = await interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
+			survey.name = `${collected.first().content}`
 
-														const params = {
-															TableName: 'comp-discord-bot',
-															Item: {
-																// Use Date.now().toString() just to generate a unique value
-																id: Date.now().toString(),
-																// `info` is used to save the actual data
-																info: survey
-															}
-														}
-														docClient.put(params, (error) => {
-															if (!error) {
-																// Finally, return a message to the user stating that the app was saved
-																return interaction.followUp(`We've successfully saved your response. Thank you!`)
-															} else {
-																throw "Unable to save record, err" + error
-															}
-														})
-													});
-											});
-									});
-							});
-					})
-					.catch(collected => {
-						interaction.followUp(`Looks like you didn't respond in time.`);
-					});
-			});
+			await interaction.followUp(`What is your favorite candy?`)
+			collected = await interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
+			survey.candy = `${collected.first().content}`
+
+			await interaction.followUp(`I'm not sure I like that candy... Why do you enjoy it so?`)
+			collected = await interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
+			survey.reason = `${collected.first().content}`
+
+			await interaction.followUp(`Fair enough! That's as good a reason as any!`)
+
+			const params = {
+				TableName: 'comp-discord-bot',
+				Item: {
+					// Use Date.now().toString() just to generate a unique value
+					id: Date.now().toString(),
+					// `info` is used to save the actual data
+					info: survey
+				}
+			}
+
+			docClient.put(params, (error) => {
+				if (!error) {
+					// Finally, return a message to the user stating that the app was saved
+					return interaction.followUp(`We've successfully saved your response. Thank you!`)
+				} else {
+					throw "Unable to save record, err" + error
+				}
+			})
+
+		} catch (err) {
+			await interaction.followUp(`Looks like you didn't respond in time.`);
+		}
 	}
 }
